d603bd6193c12d75fbf919e3f05b1f6ac056942c
MateuszSliwinski <167821763+Sliww@users.noreply.github.com>
2024-07-10 03:56:29 +0200

    Mockup Netflix quasi completato

---
+++ script.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      slidesPerView: "auto",
+      spaceBetween: 10,
+})
